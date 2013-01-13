d140e15ba44e2c197625d6229117cd093cada6f8
Paul M. Lambert <plambert@plambert.net>
2013-01-13 14:38:53 -0800

    parses dates now

--- paizo.rb
+++ paizo.rb
@@ -6,8 +6,6 @@ require 'highline/import'
 require 'logger'
 require 'irb'
 
-baseurl="http://paizo.com/"
-
 log=Logger.new(STDERR)
 
 module IRB # :nodoc:
@@ -33,13 +31,160 @@ module IRB # :nodoc:
   end
 end
 
-#class PaizoDownloadParser << Mechanize::Download
-  #def initialize uri = nil, response = nil, body = nil, code = nil
-    #super uri, response, body, code
-  #end
-#end
+class PaizoDownloader
+  attr_reader :username, :password, :agent, :download_path, :logged_in
 
+  # create a new instance
+  def initialize opts={}
+    @baseurl=opts[:url] || "http://paizo.com/"
+    @username=opts[:username] || ENV["PAIZO_USER"]
+    @password=opts[:password]
+    @log=opts[:log]
+    @agent=opts[:agent]
+    @download_path=opts[:dir] || opts[:download_path] || "#{ENV["HOME"]}/Dropbox/Paizo"
+    unless @agent
+      @agent=Mechanize.new
+    end
+    unless @log.respond_to?(:info)
+      @log=Logger.new(@log)
+    end
+    @agent.log = opts.has_key?(:agent_log) ? opts[:agent_log] : Logger.new("paizo.log")
+    @agent.user_agent_alias = "Mac Safari"
+    @agent.pluggable_parser.default=Mechanize::Download
+    @logged_in=false
 
+    @log.info "user=#{username}"
+    @log.info "pass=#{"*" * password.length}"
+    @log.info "useragent=#{@agent.user_agent}"
+
+    FileUtils.mkdir_p(@download_path)
+
+  end
+
+  # make sure we're on the homepage
+  def go_home
+    @log.info "going to homepage #{@baseurl}"
+    @agent.get(@baseurl)
+    @log.info "received #{@agent.page.body.length} bytes"
+  end
+
+  # make sure we're on the downloads page
+  def go_to_downloads
+    self.login
+    self.go_home
+    @log.info "clicking 'My Downloads' link"
+    @agent.page.link_with(:text => 'My Downloads').click
+    @log.info "received #{@agent.page.body.length} bytes"
+  end
+
+  # log into the website
+  def login
+    if ! @logged_in
+      self.go_home
+      @log.info "clicking 'Sign In'"
+      @agent.page.link_with(:text => 'Sign In').click
+      @log.info "received #{@agent.page.body.length} bytes"
+
+      unless f=@agent.page.form_with(:action => /\/signIn$/)
+        @log.error "unable to find form with signIn action"
+        exit 1
+      end
+
+      f.e=username
+      f.p=password
+
+      @log.info "submitting login form"
+      @agent.submit(f)
+      @log.info "received #{@agent.page.body.length} bytes"
+
+      if @agent.page.link_with(:text => 'Sign In')
+        @log.error "login failed; still have a 'Sign In' link"
+        exit 1
+      end
+
+      @logged_in = true
+    end
+  end
+
+  # get the timestamp from a <td><time datetime="...">...</time></td> structure
+  def timestamp_from_td td
+    if x=td.at('time')
+      Time.iso8601(x['datetime'])
+    else
+      nil
+    end
+  end
+
+  # force update the list of possible downloads
+  def refresh_downloads
+    self.go_to_downloads
+    @log.info "fetching list of downloads"
+    @downloads = Array.new
+    @agent.page.search('tr.evenOdd').each_with_index do |download_row, idx|
+      docname=download_row.at('a').text.sub(/^\s+/,'').sub(/\s+$/,'')
+      publisher=download_row.previous
+      while publisher['class'] =~ /evenOdd/
+        publisher=publisher.previous
+      end
+      publisher=publisher.at('b').text.sub(/\s+$/,'').sub(/^\s+/,'').sub(/:\s+/,'/')
+      date=Hash.new
+      date[:downloaded] = timestamp_from_td(download_row.search('td')[2])
+      date[:updated] = timestamp_from_td(download_row.search('td')[3])
+      date[:added] = timestamp_from_td(download_row.search('td')[4])
+      record = { :docname => docname, :publisher => publisher, :link => download_row.at('a'), :idx => idx, :row => download_row, :date => date }
+      @downloads.push record
+    end
+    @downloads
+  end
+
+  # return the list of possible downloads, refreshing if requested or none cached
+  def downloads refresh=false
+    if refresh or !@downloads
+      refresh_downloads
+    else
+      @downloads
+    end
+  end
+
+  # given a download or index into the downloads, attempt to save it
+  def save download
+    sleeptimes=[ 2, 4, 8, 10 ]
+    download=Integer === download ? self.downloads[download] : download
+    @log.info "starting a download of #{download[:docname]}"
+    @agent.click(download[:link])
+    link=nil
+    (sleeptimes.length + 1).times do |nth_time|
+      if nth_time == 0
+        @log.info "clicking on download link for #{download[:docname]}"
+      else
+        @log.info "retry ##{nth_time} for the download link for #{download[:docname]}"
+      end
+      if link=@agent.page.link_with(:text => 'click here', :href => /^https:/)
+        break
+      else
+        delay=sleeptimes.shift
+        @log.info "waiting #{delay} seconds for download link to be available"
+        sleep #{delay}
+        @agent.click(download[:link])
+      end
+    end
+    unless link
+      @log.error "no download link after 10 seconds, aborting!"
+      return 0
+    end
+    filename=link.href.sub(/.*\//,'')
+    @log.info "downloading #{filename}"
+    @agent.click(link)
+    filename=@agent.page.filename
+    @log.info "saving #{filename} into #{@download_path}/#{download[:publisher]}/#{download[:docname]}/"
+    @agent.page.save("#{@download_path}/#{download[:publisher]}/#{download[:docname]}/#{filename}")
+    self.refresh_downloads
+    true
+  end
+
+end
+
+# parse command line options
 
 username=ENV["PAIZO_USER"]
 password=ENV["PAIZO_PASS"]
@@ -67,83 +212,27 @@ while password.nil? or password.length == 0
   password = ask("Enter your Paizo password: ") { |q| q.echo = false }
 end
 
-log.info "user=#{username} pass=#{"*" * password.length}"
+log.info "making new PaizoDownloader object"
+paizo=PaizoDownloader.new({ :username => username, :password => password, :log => log })
 
-agent=Mechanize.new
+log.info "found #{paizo.downloads.length} download links"
 
-agent.log = Logger.new "mech.log"
-agent.user_agent_alias = "Mac Safari"
-agent.pluggable_parser.default=Mechanize::Download
-
-log.info "getting #{baseurl}"
-agent.get(baseurl)
-log.info "received #{agent.page.body.length} bytes"
-
-log.info "clicking 'Sign In'"
-agent.page.link_with(:text => 'Sign In').click
-log.info "received #{agent.page.body.length} bytes"
-
-unless f=agent.page.form_with(:action => /\/signIn$/)
-  log.error "unable to find form with signIn action"
-  exit 1
-end
-
-f.e=username
-f.p=password
-
-log.info "submitting login form"
-agent.submit(f)
-log.info "received #{agent.page.body.length} bytes"
-
-if agent.page.link_with(:text => 'Sign In')
-  log.error "login failed; still have a 'Sign In' link"
-  exit 1
-end
-
-log.info "clicking 'My Downloads' link"
-agent.page.link_with(:text => 'My Downloads').click
-log.info "received #{agent.page.body.length} bytes"
-
-def download_file(agent,log,download)
-  log.info "starting a download"
-  docname=download.at('a').text.sub(/^\s+/,'').sub(/\s+$/,'')
-  publisher=download.previous
-  while publisher['class'] =~ /evenOdd/
-    publisher=publisher.previous
+paizo.downloads.each do |download| 
+  d=download[:date]
+  msg=""
+  if download[:downloaded].nil?
+    msg = "Never downloaded"
+    download[:status]=:never
+  elsif download[:downloaded] > download[:updated]
+    msg = "Have most recent: #{download[:updated]} is older than #{download[:downloaded]}"
+    download[:status]=:old
+  else
+    msg = "Must download: #{download[:updated]} is newer than #{download[:downloaded]}"
+    download[:status]=:new
   end
-  publisher=publisher.at('b').text.sub(/\s+$/,'').sub(/^\s+/,'').sub(/:\s+/,'/')
-  agent.click(download.at('a'))
-  log.info "clicking on download link for #{docname}"
-  link=agent.page.link_with(:text => 'click here', :href => /^https:/)
-  if ! link
-    log.info "waiting 10 seconds for download link to be available"
-    sleep 10
-    agent.back
-    agent.click(download.at('a'))
-    link=agent.page.link_with(:text => 'click here', :href => /^https:/)
-    if ! link
-      log.error "no download link after 10 seconds, aborting!"
-      return 0
-    end
-  end
-  filename=link.href.sub(/.*\//,'')
-  log.info "downloading #{filename}"
-  agent.click(link)
-  log.info "saving #{filename} into Paizo/#{publisher}/#{docname}/"
-  agent.page.save("Paizo/#{publisher}/#{docname}/#{agent.page.filename}")
-  agent.back
+  puts "#{download[:idx]}: #{download[:docname]}: #{msg}"
 end
-
-def get_downloads(agent)
-  agent.page.search('tr.evenOdd')
-end
-
-FileUtils.mkdir_p('Paizo')
-
-downloads=get_downloads(agent)
-log.info "found #{downloads.length} download links"
-
-download_file(agent,log,downloads[1])
 
 IRB.start_session(binding)
+
 
